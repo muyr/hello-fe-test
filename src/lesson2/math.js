6ddfc1ad5bffe9d9ee70d931c1b043560546b911
Yanru Mu <muyanru345@163.com>
2019-11-19 15:18:32 +0800

    使用 jest 改写 lesson 1的测试

---
+++ src/lesson2/math.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b
+}
+
+function minus(a, b) {
+  return a - b
+
+}
+
+function multi(a, b) {
+  return a * b
+
+}
+
+module.exports = {
+  add,
+  minus,
+  multi
+}
